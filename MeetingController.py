44e7383e08b135151d235779486412d846ab4e40
Tim Clarke <tim.clarke@minerva.info>
2019-07-27 22:02:40 +0100

    improve search url

--- MeetingController.py
+++ MeetingController.py
@@ -12,17 +12,17 @@ import requests
 import DBQueryBuilder as qb
 
 class MeetingController():
-    
+
     def __init__(self):
         self.found_com_names = set()
         self.doc_retriever = dr()
         self._found_links = None
         self._final_com_name = set()
         self._document_data = {}
-        
+
     def updateView(self):
         return self.meeting.getAddress()
-    
+
     def getDocumentParts(self):
         resolutions = []
         for doc_number in self._document_data:
@@ -32,10 +32,10 @@ class MeetingController():
                 resolutions.append(meeting.getData())
                 # print(len(self._document_data[doc_number][doc]))
         return resolutions
-        
+
     def getMeetingData(self, search_term, start_date, end_date):
         self._getCompanyNames(search_term, start_date, end_date)
-        
+
         if self.checkOnlyOneCompany(search_term):
             final_name = set(self._final_com_name)
             self._getDocumentTextLink()
@@ -44,7 +44,7 @@ class MeetingController():
             found_names = self.found_com_names
             self.found_com_names = set() # Reset the found com names for subsequent searches
             return found_names
-        
+
     def checkOnlyOneCompany(self, searched_name):
         if not self.found_com_names: return False
         if len(self.found_com_names) > 1:
@@ -59,19 +59,19 @@ class MeetingController():
         # Only one company found so all is fine
         self._final_com_name = self.found_com_names
         return True
-    
+
     def _getCompanyNames(self, search_term, start_date, end_date):
         blacklist = ["[text]", "[html]"]
-        link="https://www.sec.gov/cgi-bin/srch-edgar?text=DEF+14A+{0}&first={1}&last={2}".format(search_term, start_date, end_date)
+        link="https://www.sec.gov/cgi-bin/srch-edgar?text=DEF+14A+company-name=({0})&first={1}&last={2}".format(search_term, start_date, end_date)
         doc_html = self.doc_retriever.getDocHTML(link)
         bs = BeautifulSoup(doc_html, 'html.parser')
         html_text = bs.prettify()
-        
+
         self._found_links = bs.select("a[href*=Archives]")
         for link in self._found_links:
             if link.contents[0] not in blacklist:
                 self.found_com_names.add(link.contents[0])
-    
+
     def _getDocumentTextLink(self):
         company_name = self._final_com_name.pop()
         document_links = []
@@ -79,9 +79,9 @@ class MeetingController():
             if link.contents[0] == company_name:
                 new_link = link.get('href').replace("-index.htm", ".txt")
                 document_links.append(new_link)
-        
+
         self._storeDocumentText(document_links)
-        
+
     def _storeDocumentText(self, document_links):
         base_url = "https://www.sec.gov"
         documents = {}
@@ -94,13 +94,13 @@ class MeetingController():
                 document_year = "20"+year
             elif int(year) >= 94:
                 document_year = "19"+year
-            
+
             text = self.doc_retriever.getDocHTML(full_link)
             document = {document_year: text}
             documents[document_number] = document
             document_number += 1
-        
+
         self._document_data = documents
-    
+
     def _storeParsedData(self):
         query_builder = qb.QueryBuilder()
